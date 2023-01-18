c04eeebf9dea63e6b7843d9d9024263d8fa0a7e7
shaheryar faraz <shaheryarfaraz9@gmail.com>
2023-01-19 00:25:42 +0500

    testing

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const userRoute = require("./routes/user.js");
 const registerRoute = require("./routes/register.js");
 const bodyParser = require("body-parser");
 
-const url = "https://smiling-ruby-cormorant.cyclic.app/";
 //middlewares
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  res.send({
+  res.json({
     message: "Welcome to CRUD API Testing",
     getAllUsers: "use /api/user to get all users",
     getUsersById: "use /api/user/:id to get specific user",
   });
 });
 app.use("/api/user", userRoute);
-app.use(url + "/api/register", registerRoute);
+app.use("/api/register", registerRoute);
 
 app.listen(8800, () => {
   console.log("Api Working!");
